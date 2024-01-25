b15f625a7c41bfd415a780a7249419bbda7cb461
Györkös Imre <88290711+brabbit1337@users.noreply.github.com>
2024-01-25 15:52:42 +0100

    Add files via upload

---
+++ 04-aur_program_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+paru -S --noconfirm --needed noto-fonts-cjk
+paru -S --noconfirm --needed tamsyn-font
+paru -S --noconfirm --needed oversteer
+paru -S --noconfirm --needed xone-dkms
+paru -S --noconfirm --needed protontricks
+paru -S --noconfirm --needed dxvk-bin
+paru -S --noconfirm --needed legendary
+paru -S --noconfirm --needed vesktop-bin
